c15cbcb98aeebeec4b741338b5d52af7102b21a8
Lostradamus <52078531+lostradamus@users.noreply.github.com>
2019-09-10 16:28:58 -0400

    added mongodb datase and get,put, post and delete operations

---
+++ public/main.js
@@ -0,0 +1,39 @@
+var update = document.getElementById('update')
+
+update.addEventListener('click', function () {
+  fetch('quotes', {
+    method: 'put',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      'name': 'Seven of Nine',
+      'quote': 'We are borg.'
+    })
+  })
+  .then(response => {
+    if (response.ok) return response.json()
+  })
+  .then(data => {
+    console.log(data)
+  })
+})
+
+var del = document.getElementById('delete')
+
+del.addEventListener('click', function () {
+  fetch('quotes', {
+    method: 'delete',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      'name': 'Seven of Nine'
+    })
+  })
+  .then(res => {
+    if (res.ok) return res.json()
+  }).
+  then(data => {
+    console.log(data)
+    window.location.reload()
+  })
+})
--- server.js
+++ server.js
@@ -3,19 +3,58 @@
        const app = express()
 
        app.use(bodyParser.urlencoded({extended: true}))
+       app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(bodyParser.json())
+       const MongoClient = require('mongodb').MongoClient
 
+           var db
+
+       MongoClient.connect('mongodb-URI', (err, database) => {
+             if (err) return console.log(err)
+             db = database
+             app.listen(3000, () => {
+               console.log('listening on 3000')
+             })
+           })
        // All your handlers here...
 
-    app.listen(3000, function() {
-      console.log('listening on 3000')
-    })
-
-    app.get('/', (req, res) => {
-          res.sendFile(__dirname + '/index.html')
-          // Note: __dirname is directory that contains the JavaScript source code. Try logging it and see what you get!
-          // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+       app.get('/', (req, res) => {
+          db.collection('quotes').find().toArray((err, result) => {
+            if (err) return console.log(err)
+            // renders index.ejs
+            res.render('index.ejs', {quotes: result})
+          })
         })
 
+        app.put('/quotes', (req, res) => {
+  db.collection('quotes')
+  .findOneAndUpdate({name: 'Tuvok'}, {
+    $set: {
+      name: req.body.name,
+      quote: req.body.quote
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
         app.post('/quotes', (req, res) => {
-              console.log(req.body)
-            })
+          db.collection('quotes').save(req.body, (err, result) => {
+            if (err) return console.log(err)
+
+            console.log('saved to database')
+            res.redirect('/')
+          })
+        })
+
+        app.delete('/quotes', (req, res) => {
+          db.collection('quotes').findOneAndDelete({name: req.body.name}, (err, result) => {
+            if (err) return res.send(500, err)
+            res.send('A seven of nine quote got deleted')
+          })
+        })
