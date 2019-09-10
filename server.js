95d8572119a65e75a391f4b1f8bd37dfe8e284e0
Lostradamus <52078531+lostradamus@users.noreply.github.com>
2019-09-10 14:16:32 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+    const express = require('express')
+       const bodyParser= require('body-parser')
+       const app = express()
+
+       app.use(bodyParser.urlencoded({extended: true}))
+
+       // All your handlers here...
+
+    app.listen(3000, function() {
+      console.log('listening on 3000')
+    })
+
+    app.get('/', (req, res) => {
+          res.sendFile(__dirname + '/index.html')
+          // Note: __dirname is directory that contains the JavaScript source code. Try logging it and see what you get!
+          // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+        })
+
+        app.post('/quotes', (req, res) => {
+              console.log(req.body)
+            })
